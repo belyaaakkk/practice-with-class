0451606b469283954efa97ae6e6dfb68ebfaeab9
belyaaakkk <106542347+belyaaakkk@users.noreply.github.com>
2023-10-05 15:23:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Engine engine = new Engine("A95", "cooling", 12,"W12");
-        Body body = new Body("Sedan", "Metal");
+        Body body = new Body("Sedan", "Silver");
         Transmission transmission = new Transmission("4wd", "automatic");
 
         Car car = new Car(body, engine, transmission);
